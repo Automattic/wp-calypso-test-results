ebeb914af9585331ece1e0be3001519945ce6e3a
Edwin Takahashi <egao@outlook.com>
2022-11-02 17:19:59 -0700

    Rework regenerate-report.sh
    Rework the bash script to work with the new schema where S3 is used to store a copy of the current report as well as the

--- scripts/regenerate-report.sh
+++ scripts/regenerate-report.sh
@@ -3,28 +3,27 @@
 # This script combines a previous Allure report with a new one to create history trend.
 #
 # The following environment variables must be set in order to use this script:
-# - ALLURE_RESULTS_DATA_BASE_PATH
-#   This is the base path for the Allure data path (eg. repo_root/data)
-# - LATEST_ALLURE_RESULT
-#   This is the latest set of results. (eg. 125/)
+# - ALLURE_RESULTS_PATH
+#   This is the base path for the Allure data path (eg. repo_root/allure_results)
+# - CURRENT_REPORT_PATH
+#   This is the path for the existing report, if any. (eg. repo_root/current_allure_report)
 # - REPORT_PATH
-#   This is the output path for the report. (eg. docs/report)
+#   This is the output path for the new report. (eg. docs/report)
 #
 #!/usr/bin/env bash
 
-if [[ ! -d "$ALLURE_RESULTS_DATA_BASE_PATH/$LATEST_ALLURE_RESULT" ]]; then
-    echo "No Allure data found under $ALLURE_RESULTS_DATA_BASE_PATH/$LATEST_ALLURE_RESULT."
-    exit 1
-fi
-
-if [[ -d "$REPORT_PATH" ]]; then
+if [[ -d "$CURRENT_REPORT_PATH" ]]; then
     echo "Copying report history to new Allure results data..."
-    mkdir -p $ALLURE_RESULTS_DATA_BASE_PATH/$LATEST_ALLURE_RESULT/history
-    cp -R $REPORT_PATH/history/ $ALLURE_RESULTS_DATA_BASE_PATH/$LATEST_ALLURE_RESULT/history
+    # Make the "history" directory in the newest Allure results directory.
+    mkdir -p $ALLURE_RESULTS_PATH/history
+    # Copy over contents of the "history" directory from the existing report.
+    cp -R $CURRENT_REPORT_PATH/history/ $ALLURE_RESULTS_PATH/history
 else 
     echo "No prior report found. Creating directory..."
+    # Fresh run. Allure will generate report without considering history.
     mkdir -p $REPORT_PATH
 fi
 
 echo "Generating report..."
-allure generate --clean $ALLURE_RESULTS_DATA_BASE_PATH/$LATEST_ALLURE_RESULT --output $REPORT_PATH
+allure generate --clean $ALLURE_RESULTS_PATH --output $REPORT_PATH
+echo "Finished generating report."
